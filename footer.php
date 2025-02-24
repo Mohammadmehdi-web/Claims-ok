60a7e247fcb0b005feacd4968fe772adbfb3688b
Mohammadmehdi-web <webauctech110@gmail.com>
2025-02-24 11:30:18 +0530

    create footer page

---
+++ footer.php
@@ -0,0 +1,128 @@
+<footer>
+         <div class="tp-footer-area bg-position pt-130 pb-40" data-background="assets/img/footer/footerbg.jpg">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 mb-70">
+                     <div class="tp-footer-widget tp-footer-col-1">
+                        <div class="tp-footer-logo mb-35">
+                           <a href="index.html"><img src="assets/img/logo/logo-2.svg" alt="logo"></a>
+                        </div>
+                        <div class="tp-footer-widget-content mb-35">
+                           <p>We work with a passion of  taking challenges most creating new ones </p>
+                        </div>
+                        <div class="tp-footer-widget-social">
+                           <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                           <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                           <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                           <a href="#"><i class="fa-brands fa-pinterest"></i></a>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 mb-70">
+                     <div class="tp-footer-widget tp-footer-col-2">
+                        <h4 class="tp-footer-widget-title mb-30">Usefull links</h4>
+                        <div class="tp-footer-widget-content">
+                           <div class="row">
+                              <div class="col-lg-6 col-md-6 col-sm-6">
+                                 <div class="tp-footer-widget-link">
+                                    <ul>
+                                       <li>
+                                          <a href="#">About us</a>
+                                       </li>
+                                       <li>
+                                          <a href="#">News</a>
+                                       </li>
+                                       <li>
+                                          <a href="#">Our team</a>
+                                       </li>
+                                       <li>
+                                          <a href="#">Contact us</a>
+                                       </li>
+                                       <li>
+                                          <a href="#">Careers</a>
+                                       </li>
+                                    </ul>
+                                 </div>
+                              </div>
+                              <div class="col-lg-6 col-md-6 col-sm-6">
+                                 <div class="tp-footer-widget-link">
+                                    <ul>
+                                       <li>
+                                          <a href="#">FAQ’s</a>
+                                       </li>
+                                       <li>
+                                          <a href="#">Service</a>
+                                       </li>
+                                       <li>
+                                          <a href="#">Project</a>
+                                       </li>
+                                       <li>
+                                          <a href="#">Privacy</a>
+                                       </li>
+                                       <li>
+                                          <a href="#">Support</a>
+                                       </li>
+                                    </ul>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-3 col-lg-4 col-md-6 mb-70  col-sm-6">
+                     <div class="tp-footer-widget tp-footer-col-3">
+                        <h4 class="tp-footer-widget-title mb-40">Contact info</h4>
+                        <div class="tp-footer-widget-content">
+                           <div class="tp-footer-active-expart mb-20">
+                              <div class="tp-footer-active-img">
+                                 <img src="assets/img/footer/author.png" alt="footer">
+                              </div>
+                              <div class="tp-footer-active-content">
+                                 <span class="tp-footer-active-name">Chat with an expert</span>
+                                 <h6 class="tp-footer-active">Active now <span></span></h6>
+                              </div>
+                           </div>
+                           <div class="tp-footer-widget-info">
+                              <a href="tel:+999212354685894"><i class="flaticon-telephone"></i> + 999 2123 5468 5894</a>
+                              <a href="mailto:insuraihelp@mail.com"><i class="fa-sharp fa-regular fa-envelope"></i> insuraihelp@mail.com</a>
+                              <a href="https://www.google.com/maps/@41.6758525,-86.2531698,18.17z"><i class="fa-sharp fa-regular fa-location-dot"></i> 2307 Beverley Rd brook<br> New York 11226 U.S.</a>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 mb-70">
+                     <div class="tp-footer-widget tp-footer-col-4">
+                        <h4 class="tp-footer-widget-title mb-30">Subscribe</h4>
+                        <div class="tp-footer-widget-content">
+                           <p class="mb-20">Get Special Offers. We Never Spam.</p>
+                        </div>
+                        <div class="tp-footer-widget-form">
+                           <form action="#">
+                              <div class="tp-footer-input">
+                                 <label for="email"><i class="fa-light fa-envelope"></i></label>
+                                 <input type="text" name="email" id="email" placeholder="Your Email">
+                                 <button  class="tp-footer-input-icon" type="submit"><i class="fa-sharp fa-regular fa-arrow-right"></i></button>
+                              </div>
+                              <div class="tp-footer-input-2">
+                                 <input type="checkbox" name="checkbox" id="checkbox">
+                                 <label for="checkbox">I agree with your terms & condition</label>
+                              </div>
+                           </form>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="tp-footer-copyright">
+            <div class="container">
+               <div class="row">
+                  <div class="col-lg-12">
+                     <div class="tp-footer-copyright-text text-center">
+                        <p>Full Copyright & Design By <a href="#"> @Theme pure</a> – <span id="year"></span></p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </footer>
