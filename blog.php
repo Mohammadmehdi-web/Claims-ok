5717d55e4afd4f1231facdbc9dbe3c13292ce905
Mohammadmehdi-web <webauctech110@gmail.com>
2025-02-24 13:40:14 +0530

    some changes

---
+++ blog.php
@@ -0,0 +1,207 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+      <meta charset="utf-8">
+      <meta http-equiv="x-ua-compatible" content="ie=edge">
+      <title>Insurai - Insurance Agency HTML Template</title>
+      <meta name="description" content="">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+       
+      <!-- Place favicon.ico in the root directory -->
+      <link rel="shortcut icon" type="image/x-icon" href="assets/img/logo/favicon.png">
+
+      <!-- CSS here -->
+      <link rel="stylesheet" href="assets/css/bootstrap.css">
+      <link rel="stylesheet" href="assets/css/animate.css">
+      <link rel="stylesheet" href="assets/css/magnific-popup.css">
+      <link rel="stylesheet" href="assets/css/font-awesome-pro.css">
+      <link rel="stylesheet" href="assets/css/ion.rangeSlider.min.css">
+      <link rel="stylesheet" href="assets/css/flaticon.css">
+      <link rel="stylesheet" href="assets/css/spacing.css">
+      <link rel="stylesheet" href="assets/css/main.css">
+   </head>
+   <body>
+
+
+      <!-- back-to-top -->
+      <div class="back-to-top-wrapper">
+         <button id="back_to_top" type="button" class="back-to-top-btn">
+            <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <path d="M11 6L6 1L1 6" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>               
+         </button>
+      </div>
+
+      <!-- Preloader Start -->
+      <div class="preloader">
+         <div class="loading-container">
+            <div class="loading"></div>
+            <div id="loading-icon"><img src="assets/img/logo/loader/loader.png" alt=""></div>
+         </div>
+      </div>
+      <!-- Preloader End -->
+
+      <?php
+         include('header.php');
+      ?>
+
+      <main>
+         <!-- breadcam-area-start -->
+         <div class="tp-breadcrumb-area tp-breadcrumb-spacing bg-position" data-background="assets/img/breadcrumb/01.jpg">
+            <div class="container">
+               <div class="row">
+                  <div class="col-lg-12">
+                     <div class="tp-breadcrumb-content">
+                        <h2 class="tp-breadcrumb-title mb-15 wow fadeInRight" data-wow-duration=".9s" data-wow-delay=".3s">Blog Details</h2>
+                        <div class="tp-breadcrumb-list wow fadeInLeft" data-wow-duration=".9s" data-wow-delay=".4s">
+                           <span><a href="index.html">Home</a></span>
+                           <span class="tp-breadcrumb-dvdr">-</span>
+                           <span class="tp-breadcrumb-title-pre">Blog Details</span>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- breadcam-area-end -->
+
+        <!-- tp-postbox-area-start -->
+         <div class="tp-postbox-area pt-120 pb-90">
+            <div class="container">
+               <div class="row">
+                  <div class="col-xl-8 col-lg-7 mb-50">
+                     <div class="tp-postbox-wrapper">
+                        <article class="tp-postbox-item ">
+                           <div class="tp-postbox-thumb mb-20">
+                              <img class="w-100" src="assets/img/blog/sidebar/07.jpg" alt="thumb">
+                           </div>
+                           <div class="tp-postbox-meta mb-15">
+                              <span><a href="#"><i class="fa-regular fa-user"></i> Md Harun</a></span>
+                              <span><i class="fa-regular fa-comment"></i>02 Comments</span>
+                           </div>
+                           <div class="tp-postbox-content-2 mb-50">
+                              <h2 class="tp-postbox-title mb-20">Insurance Transforming Challenges Into</h2>
+                              <p class="mb-30 tp-postbox-para">Curabitur luctus euismod metus, eu pellentesque mauris tempus sit amet. Proin ante odio, posuere id lacus auctor, elementum tempor tellus. Integer mattis justo eu enim tempus lacinia. Fusce vitae enim diam. Ut commodo viverra magna non egestas. Integer sodales massa at odio tristique volutpat. Proin posuere odio maximus, eleifend felis sed, ultrices turpis. Proin ultricies sodales nisl vel euismod. Praesent vestibulum sem lorem, eget fermentum justo iaculis et</p>
+                              <p class="tp-postbox-para">vitae lobortis eros purus non augue. Nullam molestie augue diam, scelerisque porta dolor mollis a. Cras condimentum elementum eros at finibus. pharetra condimentum sagittis. Donec consequat velit et nisi scelerisque, quis iaculis felis tincidunt. In faucibus sapien ut elit hendrerit, et tristique mauris lacinia. Phasellus tincidunt scelerisque lectus sed scelerisque. Donec at enim facilisis, tempus nisi quis, pharetra enim</p>   
+                           </div>
+                          
+                           <div class="tp-postbox-list">
+                            
+                              <div class="row">
+                                 <div class="col-xl-5 mb-35">
+                                    <div class="tp-postbox-list-img">
+                                       <img class="w-100" src="assets/img/blog/sidebar/08.jpg" alt="blog">
+                                    </div>
+                                 </div>
+                                 <div class="col-xl-5 mb-35">
+                                    <div class="tp-postbox-list-img">
+                                       <img class="w-100" src="assets/img/blog/sidebar/08.jpg" alt="blog">
+                                    </div>
+                                 </div>
+                              </div>
+                              
+                           </div>
+                        </article>
+                     </div>
+                  </div>
+                  <div class="col-xl-4 col-lg-5 mb-50">
+                     <div class="tp-blog-sidebar-wrapper">
+                        <div class="tp-blog-sidebar-widget tp-blog-sidebar-widget-2 tp-blog-sidebar-widget-4 mb-30">
+                           <h3 class="tp-blog-sidebar-title">Our Latest Blog</h3>
+                           <div class="tp-blog-post">
+                              <div class="tp-blog-post-thumb">
+                                 <img src="assets/img/blog/sidebar/01.jpg" alt="blog">
+                              </div>
+                              <div class="tp-blog-content">
+                                 <div class="tp-blog-meta-2">
+                                    <span><i class="fa-sharp fa-light fa-comments"></i>02 Comments</span>
+                                 </div>
+                                 <div class="tp-blog-post-title">
+                                    <h4><a href="blog-details.html">It’s easy to buildup business with us</a></h4>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="tp-blog-post">
+                              <div class="tp-blog-post-thumb">
+                                 <img src="assets/img/blog/sidebar/02.jpg" alt="blog">
+                              </div>
+                              <div class="tp-blog-content">
+                                 <div class="tp-blog-meta-2">
+                                    <span><i class="fa-sharp fa-light fa-comments"></i>02 Comments</span>
+                                 </div>
+                                 <div class="tp-blog-post-title">
+                                    <h4><a href="blog-details.html">Make your life easy with insurai banking</a></h4>
+                                 </div>
+                              </div>
+                           </div>
+                           <div class="tp-blog-post">
+                              <div class="tp-blog-post-thumb">
+                                 <img src="assets/img/blog/sidebar/03.jpg" alt="blog">
+                              </div>
+                              <div class="tp-blog-content">
+                                 <div class="tp-blog-meta-2">
+                                    <span><i class="fa-sharp fa-light fa-comments"></i>02 Comments</span>
+                                 </div>
+                                 <div class="tp-blog-post-title">
+                                    <h4><a href="blog-details.html">Strategy for norway peion fund global</a></h4>
+                                 </div>
+                              </div>
+                           </div>
+                        </div>
+                        <div class="tp-blog-sidebar-widget tp-blog-sidebar-widget-2 mb-30">
+                           <h3 class="tp-blog-sidebar-title tp-blog-sidebar-title-3">Catagories</h3>
+                           <div class="tp-blog-catagorie-wrapper">
+                              <div class="tp-blog-catagorie">
+                                 <ul>
+                                    <li>
+                                       <a href="#">Home Insurance <span><i class="fa-sharp fa-regular fa-arrow-right"></i></span></a>
+                                    </li>
+                                    <li>
+                                       <a href="#">Life Insurance <span><i class="fa-sharp fa-regular fa-arrow-right"></i></span></a>
+                                    </li>
+                                    <li>
+                                       <a href="#">Car Insurance <span><i class="fa-sharp fa-regular fa-arrow-right"></i></span></a>
+                                    </li>
+                                   
+                                    <li>
+                                       <a href="#">Health Insurance <span><i class="fa-sharp fa-regular fa-arrow-right"></i></span></a>
+                                    </li>
+                                    
+                                 </ul>
+                              </div>
+                           </div>
+                        </div>
+                        
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- tp-postbox-area-end -->
+
+
+      </main>
+
+      <!-- footer-area-start -->
+     <?php
+         include('footer.php');
+     ?>
+      <!-- footer-area-end -->
+
+      <!-- JS here -->
+      <script src="assets/js/vendor/jquery.js"></script>
+      <script src="assets/js/vendor/waypoints.js"></script>
+      <script src="assets/js/bootstrap-bundle.js"></script>
+      <script src="assets/js/ion.rangeSlider.min.js"></script>
+      <script src="assets/js/magnific-popup.js"></script>
+      <script src="assets/js/nice-select.js"></script>
+      <script src="assets/js/jarallax.js"></script>
+      <script src="assets/js/purecounter.js"></script>
+      <script src="assets/js/wow.js"></script>
+      <script src="assets/js/ajax-form.js"></script>
+      <script src="assets/js/main.js"></script>
+   </body>
+
+<!-- Mirrored from html.hixstudio.net/insurai-prev/insurai/blog-details.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 21 Feb 2025 13:02:42 GMT -->
+</html>
